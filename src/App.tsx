e3f344d868f915bcfab903df298dd82bf7e3595b
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2025-06-19 21:51:17 +0300

    hashli deploy :d

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, HashRouter } from 'react-router-dom';
 import Index from "./pages/Index";
 import About from "./pages/About";
 import BlogPost from "./pages/BlogPost";
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/yazi-dunyasi/">
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
@@ -25,7 +25,7 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
